75205d5344ac13790e001f694329b63e1c4ea1f8
Muhammad Sabir <sabir.tabi1122@gmail.com>
2025-07-29 20:27:17 +0500

    Implement Echo server

---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"strings"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", ":7007")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Server listening on:", listener.Addr().String())
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	fmt.Printf("Accepted connection from: %v\n", conn.RemoteAddr())
+
+	buffer := make([]byte, 1024)
+
+	for {
+		length, err := conn.Read(buffer)
+		if err != nil {
+			if err != io.EOF {
+				log.Println("Error reading from connection:", err)
+			} else {
+				log.Println("Error reading from connection:", err)
+			}
+
+			return
+		}
+
+		receivedData := strings.TrimSpace(string(buffer[:length]))
+		fmt.Printf("Received %d bytes: %s\n", length, receivedData)
+
+		if receivedData == "!" {
+			fmt.Fprintln(conn, "Goodbye!")
+			return
+		}
+
+		_, err = conn.Write([]byte(receivedData + "\n"))
+		if err != nil {
+			log.Println("Error writing to connection:", err)
+			return
+		}
+	}
+}
